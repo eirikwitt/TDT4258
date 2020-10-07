2ab313e72fef4ea8f0de3263ac301dd687ebc17b
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2020-10-07 14:26:35 +0200

    initial

---
+++ gpio.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "efm32gg.h"
+
+/*
+ * function to set up GPIO mode and interrupts
+ */
+void setup_gpio(void)
+{
+	/*
+	 * Example of HW access from C code: turn on joystick LEDs D4-D8 check 
+	 * efm32gg.h for other useful register definitions 
+	 */
+	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	/* enable GPIO clock */
+	*GPIO_PA_CTRL = 2;	/* set high drive strength */
+	*GPIO_PA_MODEH = 0x55555555;	/* set pins A8-15 as output */
+	*GPIO_PA_DOUT = 0x0700;	/* turn on LEDs D4-D8 (LEDs are active
+				 * low) */
+	*GPIO_PC_MODEL = 0x33333333;
+	*GPIO_PC_DOUT = 0xFF;
+}
+
+uint8_t read_buttons(void)
+{
+	return *GPIO_PC_DIN & 0xFF;
+}
