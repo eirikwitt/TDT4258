4b6f74d5f774c16e5d7583f9e91b891c37b2d497
Eirik <eirikw@live.no>
2020-10-07 16:13:14 +0200

    Created script to convert 16 bit samples to 12 bit

---
+++ sampleShift.py
@@ -0,0 +1,11 @@
+import sys
+
+with open(sys.stdin, "rb") as stdin:
+    with open(sys.stdout, "wb") as stdout:
+        sample = stdin.read(2)
+        converted = []
+        while sample:
+            converted[0] = ((sample[0] >> 4) | (sample[1] << 4)) & 255
+            converted[1] = sample[1] >> 4
+            stdout.write(bytes(converted))
+            sample = stdin.read(2)
