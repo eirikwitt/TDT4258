8616b7cb30b630cb56f6581583c49d4801462553
osvein <osvein@users.noreply.github.com>
2020-10-17 16:14:41 +0200

    test

--- ex2.c
+++ ex2.c
@@ -47,7 +47,7 @@ int main(void)
 
 	//uint32_t c = 0;
 	//while (++c) __asm volatile ("wfi");
-	*SCR = 2;
+	*SCR = 2|4;
 	while (1) {
 		__asm volatile ("wfi");
 		*GPIO_PA_DOUT = 0x3400;
@@ -58,7 +58,7 @@ int main(void)
 
 void setup_nvic(void)
 {
-	//*ISER0 = 1<<12 | 1<<11 | 1<<1;
+	*ISER0 = 1<<12 | 1<<11 | 1<<1;
 }
 
 /*
