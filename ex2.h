c1e3330c45fd5057340162ed55a5188e79948214
osvein <osvein@users.noreply.github.com>
2020-10-17 17:10:00 +0200

    comment ex2.[ch]

--- ex2.h
+++ ex2.h
@@ -1,5 +1,11 @@
 #include <stdint.h>
 
+/*
+ * objcopy creates _binary_x_raw_start and _binary_x_raw_end symbols for the
+ * start and end of the embedded binary data. SOUND_DECLARE and SOUND use
+ * preprocessor concatenation to import these symbols and create abstract
+ * Sound structs from them.
+ */
 #define SOUND_DECLARE(name) \
 extern int8_t _binary_##name##_raw_start[]; \
 extern int8_t _binary_##name##_raw_end[];
@@ -7,7 +13,7 @@ extern int8_t _binary_##name##_raw_end[];
 typedef struct {
 	const int8_t *const start;
 	const int8_t *const end;
-	const volatile int8_t * pos;
+	const volatile int8_t * pos; /* current position, or NULL if stopped */
 } Sound;
 extern volatile Sound sounds[8];
 
